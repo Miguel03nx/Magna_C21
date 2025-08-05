1a372e4b41ae8ff18724fc87a1cfcfc08d9375fe
Brayan Hernandez <brayannhc07@gmail.com>
2025-08-04 18:23:01 -0600

    feature: add tailwind and flowbite

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -12,17 +12,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="styles/style.css"> -->
-    
-    <!-- Preload -->
-    
-    <link rel="Preload" href="src/scss/base/_normalize.scss" as="style">
-    <link rel="stylesheet" href="src/scss/base/_normalize.scss">
-    
-    <link rel="Preload" href="build/css/app.css" as="style">
-    <link rel="stylesheet" href="/build/css/app.css">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link href="./styles-1.css" rel="stylesheet">
 
-    <link rel="Preload" href="/src/imglogin/MagnaC21-21.webp">
 </head>
 <body>
     
